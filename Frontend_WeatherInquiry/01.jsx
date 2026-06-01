2f55791b496683f045f639ea3e8db39f01565e06
OIL <2420547863@qq.com>
2026-06-01 20:55:17 +0800

    Implement weather inquiry feature
    Added weather fetching functionality based on city input.

--- Frontend_WeatherInquiry/01.jsx
+++ Frontend_WeatherInquiry/01.jsx
@@ -2,14 +2,30 @@ import { useState } from 'react';
 
 function App() {
   const [city, setCity] = useState('');
+  
   const [weather, setWeather] = useState(null);
+  
   async function handleSearch() {
+    
+    //Send a request
     const res = await fetch(
       "https://geocoding-api.open-meteo.com/v1/search?name=" + city
     );
+    
+    //Convert the string to a JS object
     const data = await res.json();
+
+    //Inside the data now
+    //{
+    //results: [...]
+    //}
+    //Take out the first result
     const firstResult = data.results[0];
+
+    //Show the object from Console
     console.log(firstResult);
+
+    //Finding the weather through both of the latitude and the longitude in API
     const latitude = firstResult.latitude;
     const longitude = firstResult.longitude;
     const weatherRes = await fetch(
@@ -31,6 +47,8 @@ function App() {
       />
 
       <button onClick={handleSearch} >Inquiry</button>
+
+      //If weather exists → display the content after it.
       {weather && (
       <div>
         <p>Temperature: {weather.temperature}°C</p>
